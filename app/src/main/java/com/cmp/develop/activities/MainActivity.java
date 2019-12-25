c547010ba1bb87c0b18ecf595b455791953e7a2e
Vladislav <likent253@gmail.com>
2019-12-25 20:48:18 +0200

    Created add dilemma activity

--- app/src/main/java/com/cmp/develop/activities/MainActivity.java
+++ app/src/main/java/com/cmp/develop/activities/MainActivity.java
@@ -1,21 +1,18 @@
 package com.cmp.develop.activities;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridLayout;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cmp.develop.R;
-import com.dgreenhalgh.android.simpleitemdecoration.grid.GridDividerItemDecoration;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,23 +23,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Spinner spinner = findViewById(R.id.spinnerType);
+        String[] arraySpinner = new String[] {
+                "Type"
+        };
+        Spinner s = (Spinner) findViewById(R.id.spinnerType);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+
         recyclerView = findViewById(R.id.recyclerView);
+        //recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new RVAdapter());
 
-        GridLayout gridLayout = findViewById(R.id.image_conteiner);
-        Drawable horizontalDivider = ContextCompat.getDrawable(this, R.drawable.divider_line);
-        Drawable verticalDivider = ContextCompat.getDrawable(this, R.drawable.divider_line);
+//        GridLayout gridLayout = findViewById(R.id.image_conteiner);
+//        Drawable horizontalDivider = ContextCompat.getDrawable(this, R.drawable.divider_line);
+//        Drawable verticalDivider = ContextCompat.getDrawable(this, R.drawable.divider_line);
+        
 
-        recyclerView.addItemDecoration(new GridDividerItemDecoration(horizontalDivider, verticalDivider, 4));
-        Drawable phone1 = ContextCompat.getDrawable(this, R.mipmap.test_phone_1);
-        Drawable phone2 = ContextCompat.getDrawable(this, R.mipmap.test_phone_2);
+       // recyclerView.addItemDecoration(new GridDividerItemDecoration(horizontalDivider, verticalDivider, 4));
+//        Drawable phone1 = ContextCompat.getDrawable(this, R.mipmap.test_phone_1);
+//        Drawable phone2 = ContextCompat.getDrawable(this, R.mipmap.test_phone_2);
     }
 }
 
 class RVAdapter extends RecyclerView.Adapter<RVAdapter.RVHolder>{
-    ArrayList<Drawable> images;
 
-    public RVAdapter(ArrayList<Drawable> images){
-        this.images = images;
+    public RVAdapter(){
+
     }
 
     @NonNull
@@ -50,7 +60,6 @@ class RVAdapter extends RecyclerView.Adapter<RVAdapter.RVHolder>{
     public RVHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View gridLayout = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_rv_item, parent, false);
         return new RVHolder(gridLayout);
-
     }
 
     @Override
@@ -60,7 +69,7 @@ class RVAdapter extends RecyclerView.Adapter<RVAdapter.RVHolder>{
 
     @Override
     public int getItemCount() {
-        return images.size();
+        return 2;
     }
 
     class RVHolder extends RecyclerView.ViewHolder{
